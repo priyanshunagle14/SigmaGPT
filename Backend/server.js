80fad19b4fb3fdf716846e7858886157f1366740
priyanshunagle14 <priyanshunagle2020@gmail.com>
2026-07-23 23:52:17 +0530

    Add health check endpoint for backend

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,7 @@ app.use(cors({
     credentials: true
 }));
 
+app.get('/', (req, res) => res.send('SigmaGPT backend is running'));
 app.use("/api", chatRoutes);
 app.use("/api/auth", authRoutes);
 
